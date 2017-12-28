ba5b2c5df4740fc965c91d44f837fcdea1c34c24
BeardedBear <germain.poirrier@gmail.com>
2017-12-28 09:52:39 +0100

    Fix CORS issues.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { promisify } = require("util");
 const { compileToString } = require("node-elm-compiler");
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const mkdtemp = promisify(fs.mkdtemp);
 const readdir = promisify(fs.readdir);
@@ -13,39 +14,40 @@ const writeFile = promisify(fs.writeFile);
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/", express.static('public'))
-
+app.use("/", express.static("public"));
+app.use(cors());
 function extractExercise(filePath) {
-  return readFile("./exercises/" + filePath, "utf8")
-    .then(content => {
-      const [_, rawMeta, body] = content.split("---\n");
-      const meta = rawMeta
-        .split("\n")
-        .map(line => line.split(":").map(w => w.trim()))
-        .reduce((acc, entry) => {
-          const [key, value] = entry;
-          return {...acc, [key]: value};
-        }, {});
-      return {
-        title: meta.title,
-        description: meta.description,
-        body: body.trim(),
-      };
-    });
+  return readFile("./exercises/" + filePath, "utf8").then(content => {
+    const [_, rawMeta, body] = content.split("---\n");
+    const meta = rawMeta
+      .split("\n")
+      .map(line => line.split(":").map(w => w.trim()))
+      .reduce((acc, entry) => {
+        const [key, value] = entry;
+        return { ...acc, [key]: value };
+      }, {});
+    return {
+      title: meta.title,
+      description: meta.description,
+      body: body.trim()
+    };
+  });
 }
 
 app.get("/exercises", (req, res) => {
   readdir("./exercises")
-    .then((files) => {
+    .then(files => {
       // XXX: instead of reading on every request, we should rather read it on
-      // app startup and cache it. 
+      // app startup and cache it.
       return Promise.all(files.map(extractExercise));
     })
-    .then((exercises) => {
+    .then(exercises => {
       res.send(exercises);
     })
     .catch(err => {
-      res.status(500).send({ error: "Cannot retrieve exercises: " + err.message });
+      res
+        .status(500)
+        .send({ error: "Cannot retrieve exercises: " + err.message });
     });
 });
 
@@ -55,9 +57,9 @@ app.post("/compile", (req, res) => {
   const sourceFile = app.hteTmp + "/Main.elm";
   writeFile(sourceFile, elmCode)
     .then(() => {
-      return compileToString([sourceFile], { yes: true, output: "index.html" })
+      return compileToString([sourceFile], { yes: true, output: "index.html" });
     })
-    .then(function (data) {
+    .then(function(data) {
       res.send(data.toString());
     })
     .catch(err => {
@@ -67,17 +69,16 @@ app.post("/compile", (req, res) => {
 
 function startServer() {
   return new Promise((resolve, reject) => {
-    mkdtemp(path.join(os.tmpdir(), 'hte-'))
-      .then((folder) => {
-        app.hteTmp = folder;
-        app.listen(3000, resolve);
-      });
+    mkdtemp(path.join(os.tmpdir(), "hte-")).then(folder => {
+      app.hteTmp = folder;
+      app.listen(3000, resolve);
+    });
   });
 }
 
 startServer()
   .then(() => {
-    console.log('App listening on port 3000!')
+    console.log("App listening on port 3000!");
   })
   .catch(err => {
     console.error(err);
