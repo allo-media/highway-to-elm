c8d870f6bc41ac3f4ce64075a6ada7c2bcbd37c2
Nicolas Perriault <nicolas@perriault.net>
2017-12-29 17:03:35 +0100

    Fix exercise code.

--- exercises/3/Main.elm
+++ exercises/3/Main.elm
@@ -3,13 +3,16 @@ module Main exposing (main)
 import Html exposing (..)
 
 
+things : List String
 things =
     [ "foo", "bar", "baz" ]
 
 
+listItem : String -> Html msg
 listItem smiley thing =
     li [] [ text <| thing ++ " " ++ smiley ]
 
 
+main : Html msg
 main =
     ul [] (List.map listItem things)
